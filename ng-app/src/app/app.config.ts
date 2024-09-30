b12b2d18b9064fcacd70e5e6d3d10cd64d8c8689
Denis Zenkovich <netherbox@gmail.com>
2024-09-30 17:56:59 +0100

    Update Angular to 18.2.6

--- ng-app/src/app/app.config.ts
+++ ng-app/src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { routes } from './app.routes';
 import { fakeBackendProvider } from './fake-backend-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptorsFromDi()),
     provideRouter(routes),
     fakeBackendProvider,
   ],
